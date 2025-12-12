340755d23984d684bb6274296720f945b92c6d7a
MilenDev <milenndo@gmail.com>
2025-12-12 09:10:10 +0200

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -101,7 +101,7 @@ const Header = () => {
                 <img
                   src={logo}
                   alt="Renovivo - Every Detail Matters"
-                  className="h-16 w-auto"
+                  className="h-12 w-auto"
                 />
               </Link>
             </div>
